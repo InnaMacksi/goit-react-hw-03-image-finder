2b32b87c3cf50dbd9eb66a95b8b299cb2b2790cd
Inna Makarenko <macksi@ukr.net>
2023-05-18 11:20:20 +0300

    update ImageGalleryItem.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,23 +1,27 @@
 // import React from "react";
-import ImageGalleryItem from "components/ImageGalleryItem/ImageGalleryItem";
+import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 import PropTypes from 'prop-types';
 import styles from './image-gallery.module.css';
 
 const ImageGallery = ({ items, showLargeImage }) => {
-        const elements = items.map(({ id, webformatURL, largeImageURL, tags }) => (
-          <ImageGalleryItem onClick={()=> showLargeImage({largeImageURL, tags})} key={id} src={webformatURL} alt={tags} />
-    ));
+  const elements = items.map(({ id, webformatURL, largeImageURL, tags }) => (
+    <ImageGalleryItem
+      onClick={() => showLargeImage({ largeImageURL, tags })}
+      key={id}
+      src={webformatURL}
+      alt={tags}
+    />
+  ));
 
-    return <ul className={styles.gallery}>{elements}</ul>;
-
-}
+  return <ul className={styles.gallery}>{elements}</ul>;
+};
 
 export default ImageGallery;
 
 ImageGallery.defaultProps = {
-    items: []
-}
+  items: [],
+};
 
 ImageGallery.propTypes = {
-    showLargeImage: PropTypes.func.isRequired,
+  showLargeImage: PropTypes.func.isRequired,
 };
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,11 +1,18 @@
 import PropTypes from 'prop-types';
-import styles from "./imageGalleryItem.module.css";
+import styles from './imageGalleryItem.module.css';
 
-const ImageGalleryItem = ({ src, alt }) => {
-    return <li className={styles.ImageGalleryItem}>
-          <img src={src} alt={alt} className={styles.ImageGalleryItem_image}/>
-        </li>
-}
+const ImageGalleryItem = ({ src, alt, onClick }) => {
+  return (
+    <li className={styles.ImageGalleryItem}>
+      <img
+        src={src}
+        alt={alt}
+        className={styles.ImageGalleryItem_image}
+        onClick={onClick}
+      />
+    </li>
+  );
+};
 
 ImageGalleryItem.propTypes = {
   src: PropTypes.string.isRequired,
