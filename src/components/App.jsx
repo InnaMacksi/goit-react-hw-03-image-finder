71b011de4947885e0798c3b1055d8c7607164255
Inna Makarenko <macksi@ukr.net>
2023-05-23 20:20:49 +0300

    transferred SerchQuery to App and

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,88 @@
-import { Component } from "react";
-import SearchQuery from './SearchQuery/SearchQuery'
+import { Component } from 'react';
+import { searchPhoto } from 'Api/api-search';
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Loader from './Loader/Loader';
+import Button from './Button/Button';
+import Modal from './Modal/Modal';
+import LargeImage from './LargeImage/LargeImage';
+import ButtonUp from './ButtonUp/ButtonUp';
+
 export class App extends Component{
 
-state={
-    searchWord:'',
-  }
+ state = {
+    search: '',
+    items: [],
+    loading: false,
+    error: null,
+    page: 1,
+    showModal: false,
+    largeImage: null,
+  };  
+
 
   onSubmit = (e)=>{
     e.preventDefault();
     
     this.setState({ searchWord: e.target.searchWord.value.trim() })
   }
-  render (){
+
+componentDidUpdate(prevProps, prevState) {
+    const { search, page } = this.state;
+    if (prevState.search !== search || prevState.page !== page) {
+      this.fetchPhotos();
+    } 
+  }
+  async fetchPhotos() {
+    try {
+      this.setState({loading: true});
+      const { search, page } = this.state;
+      const data = await searchPhoto(search, page);
+      this.setState(({ items }) => ({items: [...items, ...data.hits]}))
+    }
+    catch (error) {
+      this.setState({ error: error.message })
+    }
+    finally {this.setState({ loading: false })
+
+    }
+  }
+  
+  searchItems = ({ search }) => {
+    this.setState({ search, items: [], page: 1 });
+  };
+
+  loadMore = () => {
+    this.setState(({ page }) => ({ page: page + 1 }))
+    
+  }
+  
+  showLargeImage = ({ largeImageURL, tags }) => {
+    this.setState({
+      largeImage: {
+        largeImageURL,
+        tags,
+      },
+      showModal: true,
+    })
+  }
+
+  closeModal = () => {
+  this.setState({
+      largeImage: null,
+      showModal: false,
+    });
+  }
+  handleScrollUp = (e)=> {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+
+  render() {
+    const { items, loading, error, showModal, largeImage } = this.state;
+    const { searchItems, loadMore, showLargeImage, closeModal, handleScrollUp } = this;
   return (
     <div
       style={{
@@ -23,7 +94,16 @@ state={
         color: '#010101'
       }}
     >
-    <SearchQuery/>
+        <Searchbar onSubmit={searchItems} />
+        <ImageGallery items={items} showLargeImage={showLargeImage} />
+        {loading && <Loader/>}
+        {error && <p>{error}</p>}
+        {Boolean(items.length) && <Button onClick={loadMore} />}
+        {items.length > 12 && <ButtonUp onClick={handleScrollUp} />}
+        {showModal && <Modal close={closeModal}>
+          <LargeImage {...largeImage} />
+        </Modal>}
+      
     </div>
   );}
 };
